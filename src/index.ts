151a1b5c0e0dd04a27120e6e6181caae21ceaa69
xenizs <Xenizs@protonmail.com>
2021-02-05 13:35:28 -0400

    setup basic server config

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+//dependencies
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+dotenv.config();
+
+//config
+app.set('port', process.env.PORT || 4000);
+app.set('json spaces', 2);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//start server
+app.listen(app.get('port'), () => {
+    console.log(`listening on port ${app.get('port')}`)
+})
